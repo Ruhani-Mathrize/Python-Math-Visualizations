53268cb68fe9d01086de2b3da755f4121df536bf
Ruhani Kashni <ruhani.kashni@gmail.com>
2026-04-30 06:46:38 +0530

    Add Copyright Header
    Added project metadata and improved comments.

--- meru_scifi_hologram.py
+++ meru_scifi_hologram.py
@@ -1,3 +1,9 @@
+"""
+Project: The Code of Binary / Meru Prastara Visualization
+Copyright (c) 2026 Ruhani Kashni (MathRize)
+License: MIT License (See LICENSE file for details)
+YouTube: https://www.youtube.com/@MathRize
+"""
 from manim import *
 import math
 
